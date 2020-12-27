9ea7319a7537562c619abb11c9d705a2b0dd7faf
sagie gur ari <sagiegurari@gmail.com>
2020-12-27 13:51:40 +0000

    fix test header

--- tests/test.h
+++ tests/test.h
@@ -1,41 +1,15 @@
-#include "test.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef __TEST_H__
+#define __TEST_H__
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/stat.h>
 
-void test_run(void (*fn)(void))
-{
-  printf("Test ... ");
-  fn();
-  printf("Done\n");
-}
+void test_run(void (*fn)());
+void test_fail();
+void assert_true(bool);
 
+void assert_string_equal(char *, char *);
 
-void test_fail()
-{
-  printf(" Error\n");
-  exit(1);
-}
-
-
-void assert_true(bool value)
-{
-  if (!value)
-  {
-    test_fail();
-  }
-}
-
-
-void assert_num_equal(size_t value1, size_t value2)
-{
-  if (value1 != value2)
-  {
-#ifdef linux
-    printf("Assert Failed, value: %zu not equals to value: %zu", value1, value2);
 #endif
-    test_fail();
-  }
-}
 
