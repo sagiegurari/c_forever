f0f5cda01b9529c3083f52ffb4d5cafde35b0c58
sagie gur ari <sagiegurari@gmail.com>
2020-12-27 13:48:17 +0000

    ci

--- tests/test.h
+++ tests/test.h
@@ -1,14 +1,41 @@
-#ifndef __TEST_H__
-#define __TEST_H__
+#include "test.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <stdbool.h>
-#include <stddef.h>
 
-void test_run(void (*fn)());
-void test_fail();
-void assert_true(bool);
+void test_run(void (*fn)(void))
+{
+  printf("Test ... ");
+  fn();
+  printf("Done\n");
+}
 
-void assert_num_equal(size_t, size_t);
 
+void test_fail()
+{
+  printf(" Error\n");
+  exit(1);
+}
+
+
+void assert_true(bool value)
+{
+  if (!value)
+  {
+    test_fail();
+  }
+}
+
+
+void assert_num_equal(size_t value1, size_t value2)
+{
+  if (value1 != value2)
+  {
+#ifdef linux
+    printf("Assert Failed, value: %zu not equals to value: %zu", value1, value2);
 #endif
+    test_fail();
+  }
+}
 
